4b987a7dfc47a75570a3fdf358f292639a948338
boundino <boundino@gmail.com>
2025-07-01 06:42:03 +0200

    script for 2025oo

---
+++ run2025/lxplus/condor/tt-reco-checkfile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ $# -ne 5 ]]; then
+    echo "usage: ./tt-reco-checkfile.sh [input file] [output dir] [output filename] [python config] [cmssw release]"
+    exit 1
+fi
+
+INFILE=$1
+DESTINATION=$2
+OUTFILE=$3
+PYCONFIG=$4
+RELEASE=$5
+
+echo $SCRAM_ARCH
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+scram p CMSSW $RELEASE
+cd $RELEASE/src
+eval `scram runtime -sh`
+cp ../../$PYCONFIG .
+cp ../../emap_2025_full.txt .
+
+cmsRun $PYCONFIG inputFiles=$INFILE outputFile=$OUTFILE
+
+if [[ $(wc -c $OUTFILE | awk '{print $1}') -gt 700 ]]; then
+    # gfal-copy
+    # SRM_PREFIX="/mnt/T2_US_MIT/hadoop/" ; SRM_PATH=${DESTINATION#${SRM_PREFIX}} ;
+    # LD_LIBRARY_PATH='' PYTHONPATH='' gfal-copy file://$PWD/${OUTFILE} gsiftp://se01.cmsaf.mit.edu:2811/${SRM_PATH}/${OUTFILE}
+
+    # xrdcp
+    SRM_PREFIX="/eos/cms/" ; SRM_PATH=${DESTINATION#${SRM_PREFIX}} ;
+    xrdcp ${OUTFILE} root://eoscms.cern.ch//${SRM_PATH}/$OUTFILE
+fi
+
+cd ../../
+rm -rf $RELEASE
---
+++ run2025/lxplus/crabConfig.py
@@ -0,0 +1,41 @@
+#
+
+from CRABClient.UserUtilities import config
+config = config()
+config.General.transferOutputs = True
+# ==============================
+config.General.requestName = 'miniaod_241026_PhysicsHIPhysicsRawPrime0_375549_ZB'
+inputList = 'list/example_streamer.txt'
+config.Data.userInputFiles = open(inputList).readlines()
+# config.Data.inputDataset = '/HIPhysicsRawPrime9/HIRun2023A-PromptReco-v1/MINIAOD'
+# config.Data.runRange = '374354-374354'
+# config.Data.lumiMask = '/eos/user/c/cmsdqm/www/CAF/certification/Collisions22/Collisions2022HISpecial/Cert_Collisions2022HISpecial_362293_362323_Golden.json'
+# ==============================
+config.General.workArea = 'crab_projects'
+config.JobType.psetName = 'recoPbPbprime2mini_RAW2DIGI_L1Reco_RECO_PAT.py'
+
+# config.JobType.pluginName = 'PrivateMC'
+config.JobType.pluginName = 'Analysis'
+config.JobType.maxMemoryMB = 2500
+config.JobType.pyCfgParams = ['noprint']
+# config.JobType.numCores = 4
+config.JobType.allowUndistributedCMSSW = True
+
+##
+# config.Data.inputDBS = 'phys03'
+config.Data.unitsPerJob = 1 ##
+config.Data.totalUnits = -1
+config.Data.splitting = 'FileBased'
+# config.Data.splitting = 'EventAwareLumiBased'
+config.Data.publication = False
+# if publish
+config.Data.outputPrimaryDataset = 'PhysicsHIPhysicsRawPrime0'
+
+##
+config.Site.storageSite = 'T2_CH_CERN'
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/wangj/RECO2025'
+config.Site.whitelist = ['T2_CH_CERN']
+# config.Site.blacklist = ['T2_US_Nebraska','T2_US_UCSD','T2_US_Wisconsin','T2_FR_GRIF_IRFU','T3_US_Rutgers','T3_BG_UNI_SOFIA','T3_IT_Perugia']
+# config.Site.ignoreGlobalBlacklist = True
+# config.section_("Debug")
+# config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
