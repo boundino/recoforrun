544526e9003c5669818f9e8a9656d941d1a6f1b3
boundino <boundino@gmail.com>
2025-11-14 20:16:35 +0100

    add list tools

---
+++ run2025PbPb/list/keep_ls.sh
@@ -0,0 +1,22 @@
+# file:/eos/cms/store/t0streamer/Data/PhysicsHIPhysicsRawPrime43/000/387/867/run387867_ls0001_streamPhysicsHIPhysicsRawPrime43_StorageManager.dat
+
+[[ $1 == *.txt && -f $1 && $2 =~ ^[0-9]+ && $3 =~ ^[0-9]+ ]] && {
+
+    echo $1
+    
+    tmp_file=$(mktemp)
+    for i in `cat $1`; do
+        ls=${i##*_ls}
+        ls=${ls%%_stream*}
+        ls=`echo $ls | sed -E 's/^(0)+//'`
+
+        [[ $ls -ge $2 && $ls -le $3 ]] || continue
+        
+        echo $i >> $tmp_file
+    done
+
+    diff $1 $tmp_file --old-group-format=$'\e[38;2;210;106;112m%<\e[0m' --new-group-format=$'\e[38;2;132;192;127m%>\e[0m' --report-identical-files --unchanged-group-format=''
+    mv $tmp_file $1
+} || {
+    echo "Did nothing."
+}
---
+++ run2025PbPb/list/streamer_to_filelist.sh
@@ -0,0 +1,20 @@
+runnumber=$1 
+pd=${2:-PhysicsSpecialHLTPhysics0}
+
+[[ $runnumber =~ ^[0-9]{6}$ ]] && {
+
+    t_run="${runnumber:0:3}/${runnumber:3:3}"
+    outputfile=${pd}_${runnumber}.txt
+    rm $outputfile 2> /dev/null
+    echo "/eos/cms/store/t0streamer/Data/$pd/000/$t_run/"
+    for i in `ls -d /eos/cms/store/t0streamer/Data/$pd/000/$t_run/*`
+    do
+        echo "file:"$i >> $outputfile
+    done
+    # # mkdir -p /eos/cms/store/group/phys_heavyions/wangj/RECO2023/$pd/$t_run
+    echo list/$outputfile
+    # less $outputfile
+} || {
+    echo "usage: ./streamer_to_filelist.sh [run number] [stream name]"
+    echo "example: ./streamer_to_filelist.sh 375549 PhysicsHIPhysicsRawPrime0"
+}
